ebaa7b91ae4e74dff180d82c2662eedcbc10838f
vjhwong <victor.ju.hin.wong@gmail.com>
2023-06-04 11:04:25 +0200

    added arithmatic functions

---
+++ script.js
@@ -0,0 +1,35 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    if (y == 0) return "ERROR. Division by 0";
+    return x / y;
+}
+
+function operate(number1, number2, operator) {
+    switch (operator) {
+        case "+":
+            return add(number1, number2);
+        case "-":
+            return subtract(number1, number2);
+        case "*":
+            return multiply(number1, number2);
+        case "/":
+            return divide(number1, number2);
+        default:
+            break;
+    }
+}
+let number1;
+let number2;
+let operator;
+
